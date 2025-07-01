c5da2232e15f897ecce06099db9d9b182cb7b80e
Seth Rogers <sethr@cdata.com>
2025-07-01 16:38:26 -0400

    Working app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
+type Person = {
+  firstName: string;
+  lastName: string;
+};
+
 export function App() {
-  return null;
+  const people: Person[] = [
+    { firstName: "John", lastName: "Doe" },
+    { firstName: "Jane", lastName: "Smith" },
+  ];
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {people.map((person) => (
+            <tr>
+              <td>
+                <input value={person.firstName} />
+              </td>
+              <td>
+                <input value={person.lastName} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
